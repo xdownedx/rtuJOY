0e1d22c472837cc380bf025c1f77cc2d3695b5ec
Max Palehin <ars_kit@icloud.com>
2020-07-06 12:48:04 +0300

    Initial Commit

---
+++ rtuJOY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rtuJOY
+//
+//  Created by Максим Палёхин on 06.07.2020.
+//  Copyright © 2020 Максим Палёхин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
