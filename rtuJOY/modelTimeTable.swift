61cb5fd5f736519068b1c45874938578bbe835bc
Max Palehin <ars_kit@icloud.com>
2020-07-11 13:58:14 +0300

    make model cell

---
+++ rtuJOY/modelTimeTable.swift
@@ -0,0 +1,28 @@
+//
+//  modelTimeTable.swift
+//  rtuJOY
+//
+//  Created by Максим Палёхин on 11.07.2020.
+//  Copyright © 2020 Максим Палёхин. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Shedule {
+    var timeStart:String
+    var timeEnd:String
+    var typeLesson:String
+    var nameLesson:String?
+    var numberAudince:String?
+    var addressCampus:String?
+    
+    init(timeStart:String,timeEnd:String,typeLesson:String,nameLesson:String,numberAudince:String, addressCampus:String) {
+        self.timeStart = timeStart
+        self.timeEnd = timeEnd
+        self.typeLesson = typeLesson
+        self.nameLesson = nameLesson
+        self.numberAudince = numberAudince
+        self.addressCampus = addressCampus
+    }
+}
