27320f31c992e814f4e4c0c8aea8251e321e0c31
Max Palehin <ars_kit@icloud.com>
2020-07-15 11:16:13 +0300

    work model, but have trouble with index range

--- rtuJOY/Methods/network.swift
+++ rtuJOY/Methods/network.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 
+//struct parsing {
     func pars(){
         let urlString="http://api.mirea-assistant.ru/schedule?group=ktso-01-19"
         guard let url=URL(string: urlString) else {
@@ -9,22 +10,32 @@ import Foundation
         let session=URLSession(configuration: .default)
         let task = session.dataTask(with: url){data, response, error in
             if let data=data{
-                parseJSON(with: data)
+                let schedule = parseJSON(with: data)
+                print(schedule?.tuesday.count)
             }
         }
         task.resume()
     }
     
     
-    func parseJSON(with data:Data){
+     func parseJSON(with data:Data)->schedulePerWeek?{
         let decoder = JSONDecoder()
         do {
-            let schedule = try decoder.decode(gg.self, from: data)
-            print(schedule.schedule[10].info[0].name)
+            let tempSchedule = try decoder.decode(gg.self, from: data)
+            print(tempSchedule.schedule.count)
+            guard let schedule = schedulePerWeek(tempSchedule: tempSchedule) else {
+                return nil
+            }
+            return schedule
+            
         } catch let error as NSError{
             print(error.localizedDescription)
         }
+        return nil
     }
+//}
+
+   
     
 
 
--- rtuJOY/models/modelTimeTable.swift
+++ rtuJOY/models/modelTimeTable.swift
@@ -6,57 +6,94 @@
 //  Copyright © 2020 Максим Палёхин. All rights reserved.
 //
 
-//import Foundation
-//import UIKit
-//
-//
-//struct sheduleGroup:Codable {
-//    var monday: dayModel?
-//    var tuesday: dayModel?
-//    var wednesday: dayModel?
-//    var thursday: dayModel?
-//    var friday: dayModel?
-//    var saturday: dayModel?
-//}
-//
-//struct dayModel:Codable {
-//    var firstTask: task?
-//    var secondTask: task?
-//    var thirdTask: task?
-//    var fourthTask: task?
-//    var fifthTask: task?
-//    var sixthTask: task?
-//    var seventhTask: task?
-//    var eighthTask: task?
-//}
-//
-//struct task:Codable {
-//    var timeStart:String
-//    var timeEnd:String
-//    var typeLesson:String
-//    var nameLesson:String
-//    var numberAudince:String
-//    var nameTeacher:String
-//    var addressCampus:String
-//}
-//
-//
-////class Shedule {
-////    var timeStart:String
-////    var timeEnd:String
-////    var typeLesson:String
-////    var nameLesson:String?
-////    var numberAudince:String?
-////    var nameTeacher:String?
-////    var addressCampus:String?
-////
-////    init(timeStart:String,timeEnd:String,typeLesson:String,nameLesson:String,numberAudince:String,nameTeacher:String, addressCampus:String) {
-////        self.timeStart = timeStart
-////        self.timeEnd = timeEnd
-////        self.typeLesson = typeLesson
-////        self.nameLesson = nameLesson
-////        self.numberAudince = numberAudince
-////        self.nameTeacher = nameTeacher
-////        self.addressCampus = addressCampus
-////    }
-////}
+import Foundation
+import UIKit
+
+
+struct schedulePerWeek {
+    var monday: Array<task?> = []
+    var tuesday: Array<task?> = []
+    var wednesday: Array<task?> = []
+    var thursday: Array<task?> = []
+    var friday: Array<task?> = []
+    var saturday: Array<task?> = []
+    
+    
+    init?(tempSchedule:gg){
+        var i=0
+        
+        
+        while(i<(tempSchedule.schedule.count-1)){
+            switch tempSchedule.schedule[i].day {
+            case 1:
+                
+                monday[tempSchedule.schedule[i].number]!.timeStart=tempSchedule.schedule[i].interval.startTime
+                monday[tempSchedule.schedule[i].number]!.timeEnd=tempSchedule.schedule[i].interval.endTime
+                monday[tempSchedule.schedule[i].number]!.nameLesson=tempSchedule.schedule[i].info[0].name
+                monday[tempSchedule.schedule[i].number]!.typeLesson=tempSchedule.schedule[i].info[0].type
+                monday[tempSchedule.schedule[i].number]!.nameTeacher=tempSchedule.schedule[i].info[0].professor
+                monday[tempSchedule.schedule[i].number]!.numberAudince=tempSchedule.schedule[i].info[0].room
+            case 2:
+                tuesday[tempSchedule.schedule[i].number]!.timeStart+=tempSchedule.schedule[i].interval.startTime
+                tuesday[tempSchedule.schedule[i].number]!.timeEnd+=tempSchedule.schedule[i].interval.endTime
+                tuesday[tempSchedule.schedule[i].number]!.nameLesson+=tempSchedule.schedule[i].info[0].name
+                tuesday[tempSchedule.schedule[i].number]!.typeLesson+=tempSchedule.schedule[i].info[0].type
+                tuesday[tempSchedule.schedule[i].number]!.nameTeacher+=tempSchedule.schedule[i].info[0].professor
+                tuesday[tempSchedule.schedule[i].number]!.numberAudince+=tempSchedule.schedule[i].info[0].room
+            case 3:
+                wednesday[tempSchedule.schedule[i].number]!.timeStart+=tempSchedule.schedule[i].interval.startTime
+                wednesday[tempSchedule.schedule[i].number]!.timeEnd+=tempSchedule.schedule[i].interval.endTime
+                wednesday[tempSchedule.schedule[i].number]!.nameLesson+=tempSchedule.schedule[i].info[0].name
+                wednesday[tempSchedule.schedule[i].number]!.typeLesson+=tempSchedule.schedule[i].info[0].type
+                wednesday[tempSchedule.schedule[i].number]!.nameTeacher+=tempSchedule.schedule[i].info[0].professor
+                wednesday[tempSchedule.schedule[i].number]!.numberAudince+=tempSchedule.schedule[i].info[0].room
+            case 4:
+                thursday[tempSchedule.schedule[i].number]!.timeStart+=tempSchedule.schedule[i].interval.startTime
+                thursday[tempSchedule.schedule[i].number]!.timeEnd+=tempSchedule.schedule[i].interval.endTime
+                thursday[tempSchedule.schedule[i].number]!.nameLesson+=tempSchedule.schedule[i].info[0].name
+                thursday[tempSchedule.schedule[i].number]!.typeLesson+=tempSchedule.schedule[i].info[0].type
+                thursday[tempSchedule.schedule[i].number]!.nameTeacher+=tempSchedule.schedule[i].info[0].professor
+                thursday[tempSchedule.schedule[i].number]!.numberAudince+=tempSchedule.schedule[i].info[0].room
+            case 5:
+                friday[tempSchedule.schedule[i].number]!.timeStart+=tempSchedule.schedule[i].interval.startTime
+                friday[tempSchedule.schedule[i].number]!.timeEnd+=tempSchedule.schedule[i].interval.endTime
+                friday[tempSchedule.schedule[i].number]!.nameLesson+=tempSchedule.schedule[i].info[0].name
+                friday[tempSchedule.schedule[i].number]!.typeLesson+=tempSchedule.schedule[i].info[0].type
+                friday[tempSchedule.schedule[i].number]!.nameTeacher+=tempSchedule.schedule[i].info[0].professor
+                friday[tempSchedule.schedule[i].number]!.numberAudince+=tempSchedule.schedule[i].info[0].room
+            case 6:
+                saturday[tempSchedule.schedule[i].number]!.timeStart+=tempSchedule.schedule[i].interval.startTime
+                saturday[tempSchedule.schedule[i].number]!.timeEnd+=tempSchedule.schedule[i].interval.endTime
+                saturday[tempSchedule.schedule[i].number]!.nameLesson+=tempSchedule.schedule[i].info[0].name
+                saturday[tempSchedule.schedule[i].number]!.typeLesson+=tempSchedule.schedule[i].info[0].type
+                saturday[tempSchedule.schedule[i].number]!.nameTeacher+=tempSchedule.schedule[i].info[0].professor
+                saturday[tempSchedule.schedule[i].number]!.numberAudince+=tempSchedule.schedule[i].info[0].room
+            default:
+                return
+            }
+            i+=1
+
+            
+        }
+    }
+}
+
+struct task {
+    var timeStart:String
+    var timeEnd:String
+    var typeLesson:String
+    var nameLesson:String
+    var numberAudince:String
+    var nameTeacher:String
+    
+    init() {
+        timeStart=""
+        timeEnd=""
+        typeLesson=""
+        nameLesson=""
+        numberAudince=""
+        nameTeacher=""
+    }
+    
+}
+
